975941461350a22ea6e95bf51088a76bf91512d9
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-04-16 17:33:27 +0300

    insert into bears table

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 5, 'M', 'dark brown', 'sweet', 1),
+(2, 'Rowdy', 3, 'M', 'black', 'hyper', 1),
+(3, 'Tabitha', 2, 'F', 'gray', 'gentle', 1),
+(4, 'Sergeant Brown', 7, 'M', 'brown', 'stern', 1),
+(5, 'Melissa', 4, 'F', 'white', 'playful', 1),
+(6, 'Grinch', 9, 'M', 'green', 'mischievous', 0),
+(7, 'Wendy', 6, 'F', 'blonde', 'friendly', 1),
+(8, NULL, NULL, NULL, NULL, NULL, NULL);
